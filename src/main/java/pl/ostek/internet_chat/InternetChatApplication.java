9dc88b9d13a4a76dd415e5f1e3bc8c1c16c7e1ff
leszlaw <leonion121974@gmail.com>
2020-06-28 15:38:26 +0200

    Utworzenie projektu Spring MVC, dodanie rest kontrolera z endpointem /version oraz oraz dodanie service ktory czyta z pliku application.properties.

---
+++ src/main/java/pl/ostek/internet_chat/InternetChatApplication.java
@@ -0,0 +1,13 @@
+package pl.ostek.internet_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetChatApplication.class, args);
+	}
+
+}
