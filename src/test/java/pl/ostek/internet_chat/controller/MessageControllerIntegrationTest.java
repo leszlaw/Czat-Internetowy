5ddc19bc81aaf1e959d37bd56bcec28adc302f11
leszlaw <leonion121974@gmail.com>
2020-07-13 18:25:06 +0200

    Add MessageControllerIntegrationTest class with few template tests

---
+++ src/test/java/pl/ostek/internet_chat/controller/MessageControllerIntegrationTest.java
@@ -0,0 +1,77 @@
+package pl.ostek.internet_chat.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import pl.ostek.internet_chat.InternetChatApplication;
+import pl.ostek.internet_chat.model.Message;
+import pl.ostek.internet_chat.service.MessageService;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        classes = InternetChatApplication.class)
+@AutoConfigureMockMvc
+public class MessageControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private MessageService messageService;
+
+    @Test
+    public void sendMessage_MessageSent_StatusOk() throws Exception {
+        Message message = new Message("123", "Bob");
+        mvc.perform(post("/messages")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(message)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void getAllMessages_GetAllMessages_ReturnFilledJsonArray() throws Exception {
+        //given
+        Message message1 = new Message("123", "Alice");
+        Message message2 = new Message("123", "Alice");
+        Message message3 = new Message("123", "Bob");
+        messageService.sendMessage(message1);
+        messageService.sendMessage(message2);
+        messageService.sendMessage(message3);
+        //expected
+        mvc.perform(get("/messages")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", hasSize(2)))
+                .andExpect(jsonPath("$.Alice.*", hasSize(2)))
+                .andExpect(jsonPath("$.Bob.*", hasSize(1)));
+    }
+
+    @Test
+    public void getAllMessages_GetAllMessages_ReturnEmptyJsonArray() throws Exception {
+        mvc.perform(get("/messages")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", hasSize(0)));
+    }
+
+    private String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
