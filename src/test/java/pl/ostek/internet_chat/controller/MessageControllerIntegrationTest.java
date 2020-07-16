e21e696bf09490fc07bc40775caee961560db871
leszlaw <leonion121974@gmail.com>
2020-07-16 11:39:27 +0200

    Few changes in MessageControllerTest

--- src/test/java/pl/ostek/internet_chat/controller/MessageControllerIntegrationTest.java
+++ src/test/java/pl/ostek/internet_chat/controller/MessageControllerIntegrationTest.java
@@ -1,13 +1,11 @@
 package pl.ostek.internet_chat.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import pl.ostek.internet_chat.InternetChatApplication;
@@ -17,11 +15,9 @@ import pl.ostek.internet_chat.service.MessageService;
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK,
         classes = InternetChatApplication.class)
 @AutoConfigureMockMvc
@@ -31,22 +27,26 @@ public class MessageControllerIntegrationTest {
     private MockMvc mvc;
 
     @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
     private MessageService messageService;
 
     @Test
-    public void sendMessage_SendMessage_StatusOk() throws Exception {
+    public void sendMessage_CorrectMessage_StatusOk() throws Exception {
         //given
         Message message = new Message("123", "Bob");
         //when
         ResultActions result = mvc.perform(post("/messages")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(asJsonString(message)));
+                .content(objectMapper.writeValueAsString(message)));
         //then
-        result.andExpect(status().isOk());
+        result.andExpect(status().isOk())
+                .andDo(print());
     }
 
     @Test
-    public void getAllMessages_GetAllMessages_StatusOk() throws Exception {
+    public void getAllMessages_ThreeMessages_ReturnJsonArray() throws Exception {
         //given
         Message message1 = new Message("123", "Alice");
         Message message2 = new Message("123", "Alice");
@@ -59,16 +59,11 @@ public class MessageControllerIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON));
         //then
         result.andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(messageService.getAllMessages())))
                 .andExpect(jsonPath("$.*", hasSize(2)))
                 .andExpect(jsonPath("$.Alice.*", hasSize(2)))
-                .andExpect(jsonPath("$.Bob.*", hasSize(1)));
-    }
+                .andExpect(jsonPath("$.Bob.*", hasSize(2)))
+                .andDo(print());
 
-    private String asJsonString(final Object obj) {
-        try {
-            return new ObjectMapper().writeValueAsString(obj);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
     }
 }
