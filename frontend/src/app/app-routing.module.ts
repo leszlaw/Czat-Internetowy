6bc22252d0bb2e1af74fa0a770c6fa811c0d6426
leszlaw <leonion121974@gmail.com>
2020-08-14 14:47:37 +0200

    init homepage

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule , PreloadAllModules} from '@angular/router';
+import {HomeComponent} from './home/home.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '' , loadChildren: () => import('./home/home.module').then(m => m.HomeModule)}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{ preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
