a832d4ceed5d6ac30ff12a20f3762b2d419c5dde
Dardar-tech <dra135dar@gmail.com>
2020-08-25 16:47:17 +0200

    Fix web tests

--- backend/src/test/java/pl/ostek/internet_chat/controller/ContactControllerTest.java
+++ backend/src/test/java/pl/ostek/internet_chat/controller/ContactControllerTest.java
@@ -2,19 +2,18 @@ package pl.ostek.internet_chat.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import pl.ostek.internet_chat.model.dto.UserDto;
 import pl.ostek.internet_chat.service.ContactService;
 import pl.ostek.internet_chat.service.UserService;
 
-import java.security.Principal;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,14 +21,14 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(value = ContactController.class)
-@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureMockMvc
 public class ContactControllerTest {
 
     @Autowired
@@ -47,15 +46,11 @@ public class ContactControllerTest {
     @MockBean
     BCryptPasswordEncoder encoder;
 
-    @Mock
-    Principal principal;
-
     @Test
+    @WithMockUser(username="admin")
     void addContact_CorrectId_ContactAdded() throws Exception{
-        //given
-        given(principal.getName()).willReturn("admin");
         //when
-        ResultActions result = mvc.perform(post("/contacts?id=1").principal(principal));
+        ResultActions result = mvc.perform(post("/contacts?id=1").with(csrf()));
 
         result.andExpect(status().isOk())
                 .andDo(print());
@@ -64,14 +59,14 @@ public class ContactControllerTest {
     }
 
     @Test
+    @WithMockUser(username="alice")
     void getContacts_GivenContacts_ReturnJsonArray() throws Exception{
         //given
         UserDto bob=new UserDto("2","bob","bob@office.pl");
         List<UserDto> users= Arrays.asList(bob);
         given(contactService.getContacts("alice")).willReturn(users);
-        given(principal.getName()).willReturn("alice");
         //when
-        ResultActions result = mvc.perform(get("/contacts").principal(principal));
+        ResultActions result = mvc.perform(get("/contacts"));
 
         result.andExpect(status().isOk())
                 .andExpect(content().string(objectMapper.writeValueAsString(users)))
