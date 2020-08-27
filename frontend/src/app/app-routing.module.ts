f5389ae96af95944e2fad3ef4243346d830e8506
leszlaw <leonion121974@gmail.com>
2020-08-27 15:39:50 +0200

    Add login page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule , PreloadAllModules} from '@angular/router';
-import {HomeComponent} from './modules/home/home.component'
+import {HomeComponent} from './modules/home/home.component';
+import {ChatComponent} from './modules/chat/chat.component';
+import { AuthGuard } from './auth/guards/auth.guard';
 
 const routes: Routes = [
-{path: '' , loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)}
+{path: '' , loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)},
+{path: 'chat' , loadChildren: () => import('./modules/chat/chat.module').then(m => m.ChatModule), canActivate:[AuthGuard]}
 ];
 
 @NgModule({
--- frontend/src/app/modules/home/components/login/login.component.ts
+++ frontend/src/app/modules/home/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../../../shared/services/auth.service';
+import { AuthService } from '../../../../auth/services/auth.service';
+import { NgForm } from '@angular/forms';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,13 +11,17 @@ import { AuthService } from '../../../../shared/services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
-  token:string;
+  isLoginError : boolean = false;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService,private router : Router) { }
 
-  ngOnInit(): void {
-    this.authService.login('user','user').subscribe((data:any)=>{this.token=data.access_token;});
+  ngOnInit(): void {}
 
+  onSubmit(ngForm: NgForm): void{
+    this.authService.login(ngForm.value.Username,ngForm.value.Password).subscribe(
+    (data:any)=>{localStorage.setItem('token',data.access_token);
+                       this.router.navigate(['/chat']);},
+    (err : HttpErrorResponse)=>{this.isLoginError = true;});
   }
 
 }
